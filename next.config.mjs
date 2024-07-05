53560a820721e9c467843c215e9b5cbc8520bac5
Sachin9594 <43594272+Sachin9594@users.noreply.github.com>
2024-07-05 14:29:50 +0530

    First Commit

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  //   images: {
+  //     remotePatterns: [
+  //       {
+  //         protocol: "http",
+  //         hostname: "localhost",
+  //       },
+  //       {
+  //         protocol: "https",
+  //         hostname: "digitalhippo-production.up.railway.app",
+  //       },
+  //     ],
+  //   },
+  images: {
+    domains: ["localhost", "hippomarketplace.shop"],
+  },
+};
+
+export default nextConfig;
